b593c4f18b7dcd5e4d1843c6cc0881771c9d99dc
Nour <helmi.nour.91@gmail.com>
2023-08-14 17:08:11 +0300

    updates

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,13 @@
 import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+
+import "@matterlabs/hardhat-zksync-deploy";
+import "@matterlabs/hardhat-zksync-solc";
+import "@matterlabs/hardhat-zksync-verify";
+import "@matterlabs/hardhat-zksync-upgradable";
+
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+const alchemyAPIKey = "WOCmHzWzM75YJVRYkIKNkpq2KNYRyQnX";
 
 const config: HardhatUserConfig = {
   solidity: { 
@@ -7,9 +15,26 @@ const config: HardhatUserConfig = {
     settings: {
       optimizer: {
         enabled: true,
-        runs: 400,
+        runs: 200,
       },
       viaIR: true,
+    }
+  },
+  zksolc: {
+    version: "latest",
+    settings: { }
+  },
+  defaultNetwork: "zkSyncTestnet",
+  networks: {
+    hardhat: { zksync: false},
+    goerli: {
+      url: `https://eth-goerli.g.alchemy.com/v2/${alchemyAPIKey}`, // The Ethereum Web3 RPC URL (optional).
+      zksync: false, // disables zksolc compiler
+    },
+    zkSyncTestnet: {
+      url: "https://testnet.era.zksync.dev", // The testnet RPC URL of zkSync Era network.
+      ethNetwork: "goerli", // The Ethereum Web3 RPC URL, or the identifier of the network (e.g. `mainnet` or `goerli`)
+      zksync: true, // enables zksolc compiler
     }
   }
 };
